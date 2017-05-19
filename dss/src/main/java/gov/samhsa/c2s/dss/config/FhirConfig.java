1a2c3315a48717c8e695965f9d221c19137691a8
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-05-19 18:53:33 -0400

    Implement fhir validation bean
    #13517 - As a developer, I have integrated i18n support in c2s ui with backend and updated all pages in C2S UI

--- dss/src/main/java/gov/samhsa/c2s/dss/config/FhirConfig.java
+++ dss/src/main/java/gov/samhsa/c2s/dss/config/FhirConfig.java
@@ -4,6 +4,9 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.JsonParser;
 import ca.uhn.fhir.parser.XmlParser;
 import ca.uhn.fhir.validation.FhirValidator;
+import ca.uhn.fhir.validation.IValidatorModule;
+import ca.uhn.fhir.validation.SchemaBaseValidator;
+import ca.uhn.fhir.validation.schematron.SchematronBaseValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,6 +31,15 @@ public class FhirConfig {
 
     @Bean
     public FhirValidator fhirValidator() {
-        return fhirContext().newValidator();
+        FhirContext fhirContext =  fhirContext();
+        FhirValidator fhirValidator = fhirContext().newValidator();
+
+        // Create some modules and register them
+        IValidatorModule module1 = new SchemaBaseValidator(fhirContext);
+        fhirValidator.registerValidatorModule(module1);
+        IValidatorModule module2 = new SchematronBaseValidator(fhirContext);
+        fhirValidator.registerValidatorModule(module2);
+
+        return fhirValidator;
     }
 }
